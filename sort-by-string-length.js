8634e8077f9de60d07f473da095265cc2859a83f
seongkevinlee <seongkevinlee@gmail.com>
2020-10-26 21:15:19 -0700

    completed sort by string length problem

---
+++ sort-by-string-length.js
@@ -0,0 +1,11 @@
+//! Create a function that returns an array of strings sorted by length in ascending order.
+
+//? Examples
+//? sortByLength(["a", "ccc", "dddd", "bb"]) ➞ ["a", "bb", "ccc", "dddd"]
+//? sortByLength(["apple", "pie", "shortcake"]) ➞ ["pie", "apple", "shortcake"]
+//? sortByLength(["may", "april", "september", "august"]) ➞ ["may", "april", "august", "september"]
+//? sortByLength([]) ➞ []
+
+function sortByLength(arr) {
+  return arr.sort((a, b) => a.length - b.length);
+}
