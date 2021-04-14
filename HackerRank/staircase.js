292313ef9ffa41014fb1266811c749276ebe2656
slee7 <slee@catalina7ventures.com>
2021-04-14 00:09:30 -0700

    completed staircase problem

--- HackerRank/staircase.js
+++ HackerRank/staircase.js
@@ -20,6 +20,10 @@
 
 //? Print a staircase as described above.
 
-function staircase(n) {}
+function staircase(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(" ".repeat(n - i) + "#".repeat(i));
+  }
+}
 
 console.log(staircase(5));
