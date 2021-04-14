7a1d874f8029cffd350a2781419aeeb177f5b5b2
slee7 <slee@catalina7ventures.com>
2021-04-13 23:56:38 -0700

    setup the staircase problem

---
+++ HackerRank/staircase.js
@@ -0,0 +1,25 @@
+//? This is a staircase of size n = 4:
+
+//?    #
+//?   ##
+//?  ###
+//? ####
+
+//! Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+//! Write a program that prints a staircase of size .
+
+//? Function Description
+
+//? Complete the staircase function in the editor below.
+
+//? staircase has the following parameter(s):
+
+//? int n: an integer
+//? Print
+
+//? Print a staircase as described above.
+
+function staircase(n) {}
+
+console.log(staircase(5));
